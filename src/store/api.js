8a27d552d4a761f285402bf4ac1b327dc314f23a
jason westover <jwestover@nvidia.com>
2024-08-30 10:22:40 -0500

    Restoring X-Requested-With Header to match upstream behavior
    Changes added :
    	Restoring HTTP Headers expected by webserver for to identify a webui session
    	Bringing in change password check as seen on upstream.
    
    Problem : 401 Basic Auth dialog is appearing when losing session in webui.  Desired UX is a redirect to the login page (backend does this for webui sessions today).
    
    Solution : retore x-requested-with Header to identify the session as a webui session
    
    Testing : Log in, kill session, no 401 dialog, redirect to login page.
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/4194097
    
    Signed-off-by: jason westover <jwestover@nvidia.com>

--- src/store/api.js
+++ src/store/api.js
@@ -1,4 +1,5 @@
 import Axios from 'axios';
+import router from '../router';
 import { setupCache, buildWebStorage } from 'axios-cache-interceptor';
 
 //Do not change store import.
@@ -6,8 +7,8 @@ import { setupCache, buildWebStorage } from 'axios-cache-interceptor';
 //dotenv customizations.
 import store from '../store';
 
-//Axios.defaults.headers.common['Accept'] = 'application/json';
-//Axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+Axios.defaults.headers.common['Accept'] = 'application/json';
+Axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 const axiosInstance = Axios.create({
   withCredentials: true,
@@ -29,18 +30,21 @@ api.interceptors.response.use(undefined, (error) => {
 
   // TODO: Provide user with a notification and way to keep system active
   if (response.status == 401) {
-    if (response.config.url != '/') {
-      window.location = '/';
+    if (response.config.url != '/login') {
+      window.location = '/login';
       // Commit logout to remove XSRF-TOKEN cookie
       store.commit('authentication/logout');
     }
   }
 
+  // Check if action is unauthorized.
   if (response.status == 403) {
-    // Check if action is unauthorized.
-    // Toast error message will appear on screen
-    // when the action is unauthorized.
-    store.commit('global/setUnauthorized');
+    if (isPasswordExpired(response)) {
+      router.push('/change-password');
+    } else {
+      // Toast error message will appear on screen.
+      store.commit('global/setUnauthorized');
+    }
   }
 
   return Promise.reject(error);
@@ -68,6 +72,9 @@ export default {
   spread(callback) {
     return Axios.spread(callback);
   },
+  set_auth_token(token) {
+    axiosInstance.defaults.headers.common['X-Auth-Token'] = token;
+  },
 };
 
 export const getResponseCount = (responses) => {
@@ -83,4 +90,14 @@ export const getResponseCount = (responses) => {
     successCount,
     errorCount,
   };
+};
+
+export const isPasswordExpired = (response) => {
+  let extInfoMsgs = response?.data?.['@Message.ExtendedInfo'];
+  return (
+    extInfoMsgs &&
+    extInfoMsgs.find(
+      (i) => i.MessageId.split('.')[4] === 'PasswordChangeRequired',
+    )
+  );
 };
