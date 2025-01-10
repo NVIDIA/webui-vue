59c90d294d9f8b93d745e83fb658d15131654cd4
jason westover <jwestover@nvidia.com>
2025-01-10 09:13:48 -0600

    Hide all other LEDs on Inventory page for GB
    Putting in a non-upstreamable platform hack
    to hide the LEDs if only for "nvidia=gb"
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/5025166
    
    Signed-off-by: jason westover <jwestover@nvidia.com>

--- src/views/HardwareStatus/Inventory/Inventory.vue
+++ src/views/HardwareStatus/Inventory/Inventory.vue
@@ -24,7 +24,7 @@
     </page-section>
 
     <!-- System table -->
-    <table-system ref="system" v-bind:show-leds="showLeds" />
+    <table-system ref="system" v-bind:show-leds="showSystemLeds" />
 
     <!-- BMC manager table -->
     <table-bmc-manager ref="bmc" v-bind:show-leds="showLeds" />
@@ -96,6 +96,9 @@ export default {
   data() {
     return {
       showLeds:
+        (process.env.VUE_APP_ENV_NAME === 'nvidia-gb') ? false :
+        process.env.VUE_APP_HIDE_INVENTORY_LED !== 'true',
+      showSystemLeds:
         process.env.VUE_APP_HIDE_INVENTORY_LED !== 'true',
       observer: null,
       validLinks: [],
