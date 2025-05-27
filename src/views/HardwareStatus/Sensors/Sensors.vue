3f076b77b395880157a064fbd3bd64f74a55626c
Shane Lin <hslin@nvidia.com>
2025-05-27 18:06:08 +0800

    Enhance SensorsStore to filter out absent sensor states
    Changes added:
    Updated the sensor data processing logic to filter any
    sensors with a status of 'Absent', improving the accuracy
    of the sensor data being collected.
    
    Problem:
    Previously, absent sensors were included in the sensor data,
    leading to potential confusion and inaccurate reporting.
    
    Solution:
    Implement a check to include or exclude sensors marked
    as 'Absent' from the sensor data array.
    By default, sensors with state "Absent" are now hidden from the tablei
    view unless explicitly included via filters.
    When shown, absent sensors display:
    - A secondary (gray) status icon instead of "Critical"
    - Their name and "Absent" status
    - "--" for all measurement values
    - Can be toggled visible using the state filter
    
    Testing:
    Verified that only sensors with valid data are included in
    the final sensor data output.
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/5200121
    https://nvbugspro.nvidia.com/bug/5269187
    
    Signed-off-by: Shane Lin <hslin@nvidia.com>
    Signed-off-by: jason westover <jwestover@nvidia.com>

--- src/views/HardwareStatus/Sensors/Sensors.vue
+++ src/views/HardwareStatus/Sensors/Sensors.vue
@@ -1,7 +1,15 @@
 <template>
   <b-container fluid="xl">
     <page-title />
-    <b-row class="align-items-end">
+    <b-row v-if="isBusy" class="justify-content-center mb-3">
+      <b-spinner 
+        label="Spinning"
+        aria-label="Loading dump type options"
+      >
+        SPINNER
+      </b-spinner>
+    </b-row>
+    <b-row v-else class="align-items-end">
       <b-col sm="6" md="5" xl="4">
         <search
           :placeholder="$t('pageSensors.searchForSensors')"
@@ -40,7 +48,7 @@
             />
           </template>
         </table-toolbar>
-        <b-table
+        <b-table v-if="allSensors.length" 
           ref="table"
           responsive="md"
           selectable
@@ -63,7 +71,7 @@
           @filtered="onFiltered"
           @row-selected="onRowSelected($event, filteredSensors.length)"
         >
-          <!-- Checkbox column -->
+          <!-- Checkbox header -->
           <template #head(checkbox)>
             <b-form-checkbox
               v-model="tableHeaderCheckboxModel"
@@ -73,38 +81,38 @@
               <span class="sr-only">{{ $t('global.table.selectAll') }}</span>
             </b-form-checkbox>
           </template>
-          <template #cell(checkbox)="row">
+
+          <!-- Checkbox column -->
+          <template #cell(checkbox)="data">
             <b-form-checkbox
-              v-model="row.rowSelected"
-              @change="toggleSelectRow($refs.table, row.index)"
+              v-model="data.rowSelected"
+              @change="toggleSelectRow($refs.table, data.index)"
             >
               <span class="sr-only">{{ $t('global.table.selectItem') }}</span>
             </b-form-checkbox>
           </template>
 
-          <template #cell(status)="{ value }">
-            <status-icon :status="statusIcon(value)" /> {{ value }}
+          <!-- Name column -->
+          <template #cell(name)="data">
+            {{ data.value }}
           </template>
-          <template #cell(currentValue)="data">
-            {{ data.value }} {{ data.item.units }}
+
+          <!-- Status column -->
+          <template #cell(status)="data">
+            <template v-if="data.item.state === 'Absent'">
+              <status-icon status="secondary" /> {{ data.item.state }}
+            </template>
+            <template v-else>
+              <status-icon :status="statusIcon(data.value)" /> {{ data.value }}
+            </template>
           </template>
-          <template #cell(lowerCaution)="data">
-            {{ data.value }} {{ data.item.units }}
-          </template>
-          <template #cell(upperCaution)="data">
-            {{ data.value }} {{ data.item.units }}
-          </template>
-          <template #cell(lowerCritical)="data">
-            {{ data.value }} {{ data.item.units }}
-          </template>
-          <template #cell(upperCritical)="data">
-            {{ data.value }} {{ data.item.units }}
-          </template>
-          <template #cell(lowerFatal)="data">
-            {{ data.value }} {{ data.item.units }}
-          </template>
-          <template #cell(upperFatal)="data">
-            {{ data.value }} {{ data.item.units }}
+
+          <!-- Value columns (except name) -->
+          <template #cell()="data">
+            <template v-if="data.item.state === 'Absent'">--</template>
+            <template v-else>
+              {{ data.value }} {{ data.item.units }}
+            </template>
           </template>
         </b-table>
       </b-col>
@@ -133,6 +141,7 @@ import TableSortMixin from '@/components/Mixins/TableSortMixin';
 import SearchFilterMixin, {
   searchFilter,
 } from '@/components/Mixins/SearchFilterMixin';
+import { BSpinner } from 'bootstrap-vue';
 
 export default {
   name: 'Sensors',
@@ -144,6 +153,7 @@ export default {
     TableFilter,
     TableToolbar,
     TableToolbarExport,
+    'b-spinner': BSpinner,
   },
   mixins: [
     TableFilterMixin,
@@ -170,6 +180,13 @@ export default {
             this.$t('global.action.ok'),
             this.$t('global.action.warning'),
             this.$t('global.action.critical'),
+          ],
+        },
+        {
+          key: 'state',
+          label: this.$t('pageSensors.table.state'),
+          values: [
+            this.$t('global.action.absent')
           ],
         },
       ],
@@ -221,7 +238,6 @@ export default {
             formatter: this.dataFormatter,
             label: this.$t('pageSensors.table.lowerWarning'),
           },
-
           {
             key: 'currentValue',
             formatter: this.dataFormatter,
@@ -299,10 +315,21 @@ export default {
         : this.filteredSensors.length;
     },
     filteredSensors() {
-      return this.getFilteredTableData(this.allSensors, this.activeFilters);
+      // First filter out absent sensors unless explicitly included in filters
+      const showAbsent = this.activeFilters.some(filter => 
+        filter.key === 'state' && filter.values.includes(this.$t('global.action.absent'))
+      );
+      
+      const nonAbsentSensors = showAbsent 
+        ? this.allSensors 
+        : this.allSensors.filter(sensor => sensor.state !== this.$t('global.action.absent'));
+      
+      // Then apply any other active filters
+      return this.getFilteredTableData(nonAbsentSensors, this.activeFilters);
     },
   },
   created() {
+    this.isBusy = true;
     this.startLoader();
     this.$store.dispatch('sensors/getAllSensors').finally(() => {
       this.endLoader();
