cb9bd36eb64f835f05bfa5250e7f5d5a6d2befb3
Jason Westover <jwestover@nvidia.com>
2025-12-12 12:49:24 -0600

    Fix Vue 3 @change event bindings for forms
    In Bootstrap-Vue-Next (Vue 3), the @change event on BFormCheckbox
    and BFormRadio passes an Event object instead of the boolean value.
    This caused malformed API payloads like:
    
      {"LocationIndicatorActive":
        {"isTrusted": true, "_vts": 1765562875420}}
    
    instead of:
    
      {"LocationIndicatorActive": true}
    
    Replace @change with @update:model-value which correctly passes the
    new value in Vue 3/Bootstrap-Vue-Next.
    
    Components fixed:
    - OverviewInventory.vue: LED toggle
    - InventoryServiceIndicator.vue: LED toggle
    - InventoryTableSystem.vue: LED toggle
    - NetworkGlobalSettings.vue: 6 network switches
    - Policies.vue: 4 policy switches (SSH, IPMI, vTPM, RTAD)
    - TableIpv4.vue: DHCP switch
    - TableIpv6.vue: DHCPv6 switch
    - Ldap.vue: LDAP auth and service type controls
    
    Also adds safety net in api.js:
    - Request interceptor to strip Vue reactivity from payloads
    - Detects and warns about Event objects in API payloads
    - Improved response error handling with null safety
    - Conditional debug logging (development mode only)
    
    Change-Id: I180d9143087284e28c5066a6ffc141cd7f7038c6
    Signed-off-by: jason westover <jwestover@nvidia.com>

--- src/views/Settings/Network/NetworkGlobalSettings.vue
+++ src/views/Settings/Network/NetworkGlobalSettings.vue
@@ -33,7 +33,7 @@
               v-model="useDomainNameState"
               data-test-id="networkSettings-switch-useDomainName"
               switch
-              @change="changeDomainNameState"
+              @update:model-value="changeDomainNameState"
             >
               <span v-if="domainState">
                 {{ $t('global.status.enabled') }}
@@ -47,7 +47,7 @@
               v-model="useDomainNameStateIpv6"
               data-test-id="networkSettings-switch-useDomainNameIpv6"
               switch
-              @change="changeDomainNameStateIpv6"
+              @update:model-value="changeDomainNameStateIpv6"
             >
               <span v-if="domainStateIpv6">
                 {{ $t('global.status.enabled') }}
@@ -66,7 +66,7 @@
               v-model="useDnsState"
               data-test-id="networkSettings-switch-useDns"
               switch
-              @change="changeDnsState"
+              @update:model-value="changeDnsState"
             >
               <span v-if="dnsState">
                 {{ $t('global.status.enabled') }}
@@ -80,7 +80,7 @@
               v-model="useDnsStateIpv6"
               data-test-id="networkSettings-switch-useDnsIpv6"
               switch
-              @change="changeDnsStateIpv6"
+              @update:model-value="changeDnsStateIpv6"
             >
               <span v-if="dnsStateIpv6">
                 {{ $t('global.status.enabled') }}
@@ -99,7 +99,7 @@
               v-model="useNtpState"
               data-test-id="networkSettings-switch-useNtp"
               switch
-              @change="changeNtpState"
+              @update:model-value="changeNtpState"
             >
               <span v-if="ntpState">
                 {{ $t('global.status.enabled') }}
@@ -113,7 +113,7 @@
               v-model="useNtpStateIpv6"
               data-test-id="networkSettings-switch-useNtpIpv6"
               switch
-              @change="changeNtpStateIpv6"
+              @update:model-value="changeNtpStateIpv6"
             >
               <span v-if="ntpStateIpv6">
                 {{ $t('global.status.enabled') }}
